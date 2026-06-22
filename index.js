54e778333072907ffc315f72da16ef07970c9b2b
akshay <aspect12007@gmail.com>
2026-06-22 23:41:53 +0530

    Initial commit: Set up Express API route

--- index.js
+++ index.js
@@ -1,80 +1,122 @@
 const express = require("express")
-const mock_data = require("./MOCK_DATA.json")
 const PORT = 8000
 const fs = require("fs")
 const app = express()
+const mongoose = require("mongoose")
 
 
-app.use(express.urlencoded({extended:false}))
+//connection
+mongoose.connect("mongodb://127.0.0.1:27017/api-app").then(() => console.log("MongoDB connected")).catch((err) => console.log(err))
 
-app.get("/users", (req, res) => {
 
-    const html_text = `
-   
-    <ul>
-    ${mock_data.map((val, ind) => `<li>${val.first_name} ${val.last_name}</li>`).join("")}
-    <ul>`
+//schema
 
-    res.send(html_text)
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+
+    },
+    email: {
+
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jobTitle: {
+        type: String,
+    },
+    gender: {
+        type: String
+    }
+})
+
+
+const User = mongoose.model('user', userSchema)
+
+
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/users", async (req, res) => {
+const allDbUsers = await User.find({})
+    const html = `<ul>
+    ${allDbUsers.map((users)=>`<li>${users.firstName}- ${users.email}</li>`).join("")}
+    </ul>
+    `
+
+    res.send(html)
 
 
 })
-app.get("/api/users" , (req,res)=>
-{
-   return res.json(mock_data)
+app.get("/api/users", async(req, res) => {
+    const allDbUsers = await User.find({})
+
+    return res.json(allDbUsers)
 })
-app.post("/api/users", (req,res)=>{
+app.post("/api/users", async (req, res) => {
+
     const body = req.body
-    mock_data.push({...body,id : mock_data.length + 1})
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(mock_data),(err)=>{
-        if(err) return res.send("505 error")
-    })
-    return res.send(`Added user ${body.first_name}`)
 
+
+
+
+
+    if (!body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title) {
+        return res.status(400).json({ msg: "All fields are req ... " })
+    }
+
+    const result = await User.create({
+        firstName: body.first_name,
+        laštName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title,
+    })
+    console.log(`result: ${result}`)
+    res.status(201).json({msg: "success"})
 })
 
 
-app.route("/api/users/:id").get((req,res)=>{
-    const id = Number(req.params.id)
-    const user = mock_data.find((user)=>user.id == id)
+app.route("/api/users/:id").get( async (req, res) => {
+    const user = await User.findById(req.params.id)
     return res.json(user)
 })
-.patch((req,res)=>{
-    const body =req.body
-    const id = Number(req.params.id)
-    const user =mock_data.find((user)=>user.id == id)
+    .patch(async(req, res) => {
+        const body = req.body
 
-    if(body.first_name)
-        user.first_name = body.first_name
-    if(body.last_name)
-         user.last_name = body.last_name
-    if(body.job_title)
-        user.job_title = body.job_title
-    if(body.email)
-        user.email = body.email
-    if (body.gender)
-        user.gender = body.gender
+        if (body.first_name)
+            await User.findByIdAndUpdate(req.params.id  , {firstName: body.first_name})
+        if (body.last_name)
+            await User.findByIdAndUpdate(req.params.id  , {lastName: body.last_name})
+        if (body.job_title)
+            await User.findByIdAndUpdate(req.params.id  , {jobTitle: body.job_title})
+        if (body.email)
+            await User.findByIdAndUpdate(req.params.id  , {email: body.email})
+        if (body.gender)
+            await User.findByIdAndUpdate(req.params.id  , {gender: body.gender})
 
-fs.writeFile("./MOCK_DATA.json",JSON.stringify(mock_data),(err)=>{
-        if(err) return res.send("505 error")
+        else
+            res.status(400)
+
+        res.json({status: "sucess"})
+
+
     })
-    return res.send(`Updated user ${id}`)
-})
-.delete((req,res)=>{
-    const body = req.body
-    const id = Number(req.params.id)
-    const userToDelete = mock_data.find(user => user.id === id);
-    const newMockData = mock_data.filter(user => user.id !== id);
-    
-     fs.writeFile("./MOCK_DATA.json",JSON.stringify(newMockData),(err)=>{
-        if(err) return res.status(505)
-        
+    .delete(async(req, res) => {
+       
+        await User.findByIdAndDelete(req.params.id)
+        res.json({status: "sucess"})
+
     })
-        return res.send(`Deleted user ${body.first_name}\n
-            User id ${req.params.id}`)
-
-
-})
 
 
 
